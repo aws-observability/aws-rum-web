db8526d00f825591af574428dff17b388db46013
Quinn Hanam <hanquinn@amazon.com>
2021-11-02 08:34:25 -0700

    Store creds in localStorage instead of cookies. (#11)

---
+++ src/loader/loader-ingestion.js
@@ -0,0 +1,23 @@
+import { loader } from './loader';
+import { showIntegRequestClientBuilder } from '../test-utils/integ-http-handler';
+import { JsErrorPlugin } from '../plugins/event-plugins/JsErrorPlugin';
+loader(
+    'cwr',
+    '[applicationId]',
+    '[applicationName]',
+    '1.0',
+    'us-west-2',
+    './rum_javascript_telemetry.js',
+    {
+        allowCookies: true,
+        clientBuilder: showIntegRequestClientBuilder,
+        dispatchInterval: 0,
+        endpoint: '[endpoint]',
+        guestRoleArn: '[guestRoleArn]',
+        identityPoolId: '[identityPoolId]',
+        metaDataPluginsToLoad: [],
+        eventPluginsToLoad: [new JsErrorPlugin()],
+        sessionSampleRate: 1,
+        telemetries: []
+    }
+);
