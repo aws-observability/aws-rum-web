4321d7bca1549aaa8dc1c72948e9834c0e524004
Quinn Hanam <hanquinn@amazon.com>
2021-11-04 15:08:05 -0700

    Transpile to ES5

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,9 +1,68 @@
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
+const babelLoaderOptions = {
+    presets: [
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    browsers: [
+                        'last 3 chrome versions',
+                        'last 3 firefox versions',
+                        'ie 11',
+                        'last 3 edge versions',
+                        'last 3 safari versions'
+                    ]
+                }
+            }
+        ]
+    ],
+    plugins: [
+        [
+            '@babel/plugin-transform-runtime',
+            {
+                absoluteRuntime: false,
+                corejs: false,
+                helpers: true,
+                regenerator: true
+            }
+        ]
+    ]
+};
+
 module.exports = {
     entry: './src/index-browser.ts',
+    target: ['web', 'es5'],
     resolve: {
         extensions: ['.ts', '.js', '.json']
     },
-    plugins: [new CaseSensitivePathsPlugin()]
+    plugins: [new CaseSensitivePathsPlugin()],
+    module: {
+        rules: [
+            {
+                test: [/\.js$/],
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: babelLoaderOptions
+                }
+            },
+            {
+                test: [/\.ts$/],
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: babelLoaderOptions
+                    },
+                    {
+                        loader: 'ts-loader',
+                        options: {
+                            configFile: 'tsconfig.webpack.json'
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 };
