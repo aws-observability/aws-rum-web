b9f2c1969fd39d20fb86fda6a23a2e3bc06e3f4d
Dinesh Reddy Chandupatla <direddyz@amazon.com>
2021-11-29 17:46:23 -0800

    update webvital tests to skip if browser is safari or firefox

--- src/plugins/event-plugins/__integ__/WebVitalsPlugin.test.ts
+++ src/plugins/event-plugins/__integ__/WebVitalsPlugin.test.ts
@@ -37,6 +37,8 @@ const removeUnwantedEvents = (json: any) => {
 test('WebVitalEvent records lcp and cls events', async (t: TestController) => {
     // If we click too soon, the client/event collector plugin will not be loaded and will not record the click.
     // This could be a symptom of an issue with RUM web client load speed, or prioritization of script execution.
+    let browser = t.browser.name;
+    if (browser != 'Safari' && browser != 'Firefox') {
     await t.wait(300);
 
     await t
@@ -65,4 +67,5 @@ test('WebVitalEvent records lcp and cls events', async (t: TestController) => {
         .eql(CLS_EVENT_TYPE)
         .expect(eventDetails2.value)
         .typeOf('number');
+    }
 });
