56502296d956a5f30b5f03e3f6f4e1487ce60ca3
Quinn Hanam <hanquinn@amazon.com>
2021-12-13 20:35:24 -0800

    Add update version types to deployment workflow. (#63)
    * Add update version types to deployment workflow.
    
    * Move deployment to script file.
    
    * Read release version from package.json

---
+++ .github/scripts/deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+version=$(npm pkg get version | sed 's/"//g')
+bucket=$1
+
+aws s3api put-object --bucket $bucket --key "content/$version/cwr.js" --body build/assets/cwr.js
+aws s3api put-object --bucket $bucket --key "content/$version/LICENSE-THIRD-PARTY" --body LICENSE-THIRD-PARTY
+
+if [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]
+then
+    minorUpdate=$(echo $version | sed -En "s/^([0-9]+\.)[0-9]+\.[0-9]+/\1x/p")
+    aws s3api put-object --bucket $bucket --key "content/$minorUpdate/cwr.js" --body build/assets/cwr.js
+    aws s3api put-object --bucket $bucket --key "content/$minorUpdate/LICENSE-THIRD-PARTY" --body LICENSE-THIRD-PARTY
+    patchUpdate=$(echo $version | sed -En "s/^([0-9]+\.[0-9]+\.)[0-9]+/\1x/p")
+    aws s3api put-object --bucket $bucket --key "content/$patchUpdate/cwr.js" --body build/assets/cwr.js
+    aws s3api put-object --bucket $bucket --key "content/$patchUpdate/LICENSE-THIRD-PARTY" --body LICENSE-THIRD-PARTY
+fi
