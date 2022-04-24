73d795e220bfbadfc235cb142fd078abd6d599f0
Reno Seo <renoseo@amazon.com>
2022-04-23 23:15:15 -0700

    test: Add commands for local development, update test cases, and parallelize tests

--- playwright.local.config.ts
+++ playwright.local.config.ts
@@ -1,4 +1,4 @@
-// playwright.config.js
+// playwright.local.config.js
 // @ts-check
 const { devices } = require('@playwright/test');
 
@@ -6,6 +6,12 @@ const config = {
     forbidOnly: !!process.env.CI,
     testDir: 'src/__smoke-test__',
     retries: process.env.CI ? 2 : 0,
+    webServer: {
+        command: 'npm run server',
+        url: 'http://localhost:8080/',
+        timeout: 120 * 1000,
+        reuseExistingServer: !process.env.CI
+    },
     use: {
         trace: 'on-first-retry'
     },
