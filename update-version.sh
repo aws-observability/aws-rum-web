55d9a628ae385fb92d37f8b6e7240b6f75146f9b
Grace Lim <limhye@amazon.com>
2023-01-26 14:21:10 -0800

    Update file path

--- update-version.sh
+++ update-version.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-version=$(jq -r '.version' package.json); jq --arg version $version '.version |= $version' version.json >file.json.tmp && cp file.json.tmp version.json; rm -f file.json.tmp
+version=$(jq -r '.version' package.json)
+jq --arg version $version '.version |= $version' version.json >file.json.tmp && cp file.json.tmp version.json
+rm -f file.json.tmp
