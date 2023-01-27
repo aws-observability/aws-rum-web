2ff1602ce9a1ed4551a34f8d6d551d0d999c794d
Grace Lim <limhye@amazon.com>
2023-01-27 11:50:27 -0800

    Update version

--- update-version.sh
+++ update-version.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 version=$(jq -r '.version' package.json)
-jq --arg version $version '.version |= $version' version.json >file.json.tmp && cp file.json.tmp version.json
-rm -f file.json.tmp
+jq -n --arg version $version '{ "version" : $version}' > version.json
