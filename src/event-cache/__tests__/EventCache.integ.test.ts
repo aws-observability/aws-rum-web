e49da7b0b254b0287b49f22d6b9f0bae5a423682
Hye Jin (Grace) Lim <94998916+limhjgrace@users.noreply.github.com>
2023-03-07 11:17:47 -0800

    fix: Hardcode webclient version (#381)
    * fix: Hardcode webclient version
    
    * fix: Update test variable value
    
    * fix: update webclient version
    
    * fix: revert tsconfig changes related to webclient versioning feature
    
    * fix: remove version updating

--- src/event-cache/__tests__/EventCache.integ.test.ts
+++ src/event-cache/__tests__/EventCache.integ.test.ts
@@ -6,11 +6,7 @@ import { DEFAULT_CONFIG, mockFetch } from '../../test-utils/test-utils';
 import { SESSION_START_EVENT_TYPE } from '../../sessions/SessionManager';
 import { INSTALL_MODULE } from '../../utils/constants';
 
-jest.mock('../../../version.json', () => ({
-    version: '2.0.0' // WEB_CLIENT_VERSION
-}));
-
-const WEB_CLIENT_VERSION = '2.0.0';
+const WEB_CLIENT_VERSION = '1.13.4';
 
 global.fetch = mockFetch;
 describe('EventCache tests', () => {
