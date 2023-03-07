2576f91a263a9a1d95ccb34db48809d09d42526d
Hye Jin (Grace) Lim <94998916+limhjgrace@users.noreply.github.com>
2023-03-07 11:41:01 -0800

    fix: Hardcode webclient version (#381) (#384)
    * fix: Hardcode webclient version
    
    * fix: Update test variable value
    
    * fix: update webclient version
    
    * fix: revert tsconfig changes related to webclient versioning feature
    
    * fix: remove version updating

--- src/event-cache/__tests__/EventCache.test.ts
+++ src/event-cache/__tests__/EventCache.test.ts
@@ -26,11 +26,7 @@ jest.mock('../../sessions/SessionManager', () => ({
     }))
 }));
 
-jest.mock('../../../version.json', () => ({
-    version: '2.0.0' // WEB_CLIENT_VERSION
-}));
-
-const WEB_CLIENT_VERSION = '2.0.0';
+const WEB_CLIENT_VERSION = '1.13.4';
 
 describe('EventCache tests', () => {
     beforeAll(() => {
