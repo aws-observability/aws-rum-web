a5fd76ac9327a15852667369f23a00eb5c06baf7
Paras Sakharkar <44929425+ps863@users.noreply.github.com>
2023-03-16 16:00:46 -0700

    Update src/sessions/PageManager.ts
    Co-authored-by: Quinn Hanam <qhanam@gmail.com>

--- src/sessions/PageManager.ts
+++ src/sessions/PageManager.ts
@@ -107,14 +107,14 @@ export class PageManager {
             this.createLandingPage(pageId);
         } else if (this.page.pageId !== pageId) {
             this.createNextPage(this.page, pageId);
+        } else if (this.resumed) {
+            // Update attributes state in PageManager for event metadata
+            this.collectAttributes(
+                this.page as Page,
+                typeof payload === 'object' ? payload : undefined
+            );
+            return;
         } else {
-            // If its a resumed or reloaded page, update attributes state in PageManager for event metadata
-            if (this.resumed) {
-                this.collectAttributes(
-                    this.page as Page,
-                    typeof payload === 'object' ? payload : undefined
-                );
-            }
             // The view has not changed.
             return;
         }
