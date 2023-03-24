bcfb12f83b1e4b475ca84d15185433bd48f0090e
Paras Sakharkar <44929425+ps863@users.noreply.github.com>
2023-03-23 20:58:58 -0700

    Update playwright.config.ts
    Co-authored-by: Quinn Hanam <qhanam@gmail.com>

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,42 +2,24 @@
 // @ts-check
 const { devices } = require('@playwright/test');
 
-if (process.env.INSTALL_METHOD === 'CDN') {
-    var config = {
-        forbidOnly: !!process.env.CI,
-        reporter: 'list',
-        workers: process.env.CI ? 4 : undefined,
-        testDir: 'src/__smoke-test__',
-        retries: process.env.CI ? 2 : 2,
-        timeout: 300000,
-        use: {
-            trace: 'on-first-retry'
-        },
-        projects: [
-            {
-                name: 'chromium',
-                use: { ...devices['Desktop Chrome'] }
-            }
-        ]
-    };
-} else {
-    var config = {
-        forbidOnly: !!process.env.CI,
-        reporter: 'list',
-        workers: process.env.CI ? 4 : undefined,
-        testDir: 'src/__smoke-test-npm__',
-        retries: process.env.CI ? 2 : 2,
-        timeout: 300000,
-        use: {
-            trace: 'on-first-retry'
-        },
-        projects: [
-            {
-                name: 'chromium',
-                use: { ...devices['Desktop Chrome'] }
-            }
-        ]
-    };
-}
+var config = {
+    forbidOnly: !!process.env.CI,
+    reporter: 'list',
+    workers: process.env.CI ? 4 : undefined,
+    testDir: process.env.INSTALL_METHOD === 'CDN' ?
+        'src/__smoke-test__' :
+        'src/__smoke-test-npm__',
+    retries: process.env.CI ? 2 : 2,
+    timeout: 300000,
+    use: {
+        trace: 'on-first-retry'
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome'] }
+        }
+    ]
+};
 
 module.exports = config;
