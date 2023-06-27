7c9f8db5595baa60d9de6a7dfbff9d9168153c94
Billy <billyzh@amazon.com>
2023-06-27 09:28:57 -0700

    fix: remove unecessary defaultIgnore() tests

--- src/plugins/utils/__tests__/performance-utils.test.ts
+++ src/plugins/utils/__tests__/performance-utils.test.ts
@@ -1,13 +1,12 @@
 import { defaultIgnore } from '../performance-utils';
 import {
     mockPerformanceEntry,
-    mockPerformanceNavigationTiming,
-    mockPerformanceResourceTiming
+    mockPerformanceNavigationTiming
 } from '../../../test-utils/mock-data';
 
 describe('performance-utils', () => {
     describe('defaultPerformanceIgnore', () => {
-        test('when resource entry is PerformanceEntry and has non-http(s) URL schema then entry is ignored', () => {
+        test('when entry has a non-http URL schema then entry is ignored', async () => {
             const mockEntry = mockPerformanceEntry({
                 name: 'chrome-extension://localhost',
                 entryType: 'resource'
@@ -15,7 +14,7 @@ describe('performance-utils', () => {
             const result = defaultIgnore(mockEntry);
             expect(result).toBe(true);
         });
-        test('when resource entry is PerformanceEntry and has http URL schema then entry is not ignored', () => {
+        test('when entry is has an http URL schema then entry is not ignored', async () => {
             const mockEntry = mockPerformanceEntry({
                 name: 'http://localhost',
                 entryType: 'resource'
@@ -23,7 +22,7 @@ describe('performance-utils', () => {
             const result = defaultIgnore(mockEntry);
             expect(result).toBe(false);
         });
-        test('when resource entry is PerformanceEntry and has https URL schema then entry is not ignored', () => {
+        test('when entry is has an https URL schema then entry is not ignored', async () => {
             const mockEntry = mockPerformanceEntry({
                 name: 'https://localhost',
                 entryType: 'resource'
@@ -32,49 +31,9 @@ describe('performance-utils', () => {
             expect(result).toBe(false);
         });
 
-        test('when entry is PerformanceResourceTiming and has non-http(s) URL-schema then entry is ignored', () => {
-            const mockEntry = mockPerformanceResourceTiming({
-                name: 'chrome-extension://localhost'
-            });
-            const result = defaultIgnore(mockEntry);
-            expect(result).toBe(true);
-        });
-
-        test('when entry is PerformanceResourceTiming and has http URL schema then the entry is not ignored', () => {
-            const mockEntry = mockPerformanceResourceTiming({
-                name: 'http://localhost'
-            });
-            const result = defaultIgnore(mockEntry);
-            expect(result).toBe(false);
-        });
-
-        test('when entry is PerformanceResourceTiming and has https URL schema then entry is not ignored', () => {
-            const mockEntry = mockPerformanceResourceTiming({
-                name: 'https://localhost'
-            });
-            const result = defaultIgnore(mockEntry);
-            expect(result).toBe(false);
-        });
-
-        test('when entry is PerformanceNavigationTiming and has non-http(s) URL schema request then entry is not ignored', () => {
+        test('when entry is not PerformanceResourceTiming then the entry is not ignored', async () => {
             const mockEntry = mockPerformanceNavigationTiming({
                 name: 'chrome-extension://localhost'
-            });
-            const result = defaultIgnore(mockEntry);
-            expect(result).toBe(false);
-        });
-
-        test('when entry is PerformanceNavigationTiming and has http URL schema then entry is not ignored', () => {
-            const mockEntry = mockPerformanceNavigationTiming({
-                name: 'http://localhost'
-            });
-            const result = defaultIgnore(mockEntry);
-            expect(result).toBe(false);
-        });
-
-        test('when entry is PerformanceNavigationTiming and has https URL schema then entry is not ignored', () => {
-            const mockEntry = mockPerformanceNavigationTiming({
-                name: 'https://localhost'
             });
             const result = defaultIgnore(mockEntry);
             expect(result).toBe(false);
