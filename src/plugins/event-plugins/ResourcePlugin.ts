15eb2f3a111bedf5e63ca4c70b970783a799eb1b
Billy <billyzh@amazon.com>
2023-09-06 09:14:11 -0700

    chore: only publish image resource events

--- src/plugins/event-plugins/ResourcePlugin.ts
+++ src/plugins/event-plugins/ResourcePlugin.ts
@@ -133,7 +133,7 @@ export class ResourcePlugin extends InternalPlugin {
                 eventData
             );
 
-            if (parsedEvent) {
+            if (parsedEvent && fileType === ResourceType.IMAGE) {
                 this.context?.bus.dispatch(PERFORMANCE_RESOURCE_EVENT_TYPE, {
                     key: entryData,
                     payload: parsedEvent
