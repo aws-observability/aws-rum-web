33892c5e091735e9449ab3683868cde1c89ac163
Billy <billyzh@amazon.com>
2023-09-11 09:59:31 -0700

    feat: add attributions for core web vitals: LCP, CLS, and FID (#432)
    * chore: update LCP, CLS, and FID schemas with attributions
    
    * feat: collect attributions for LCP, CLS, and FID
    
    * chore: remove attribution double check
    
    * chore: add intetgration test for attribution

--- src/plugins/event-plugins/WebVitalsPlugin.ts
+++ src/plugins/event-plugins/WebVitalsPlugin.ts
@@ -1,12 +1,20 @@
 import { InternalPlugin } from '../InternalPlugin';
 import { LargestContentfulPaintEvent } from '../../events/largest-contentful-paint-event';
-import { FirstInputDelayEvent } from '../../events/first-input-delay-event';
 import { CumulativeLayoutShiftEvent } from '../../events/cumulative-layout-shift-event';
-import { Metric, onCLS, onFID, onLCP } from 'web-vitals';
+import { FirstInputDelayEvent } from '../../events/first-input-delay-event';
 import {
-    LCP_EVENT_TYPE,
+    CLSMetricWithAttribution,
+    FIDMetricWithAttribution,
+    LCPMetricWithAttribution,
+    Metric,
+    onCLS,
+    onFID,
+    onLCP
+} from 'web-vitals/attribution';
+import {
+    CLS_EVENT_TYPE,
     FID_EVENT_TYPE,
-    CLS_EVENT_TYPE
+    LCP_EVENT_TYPE
 } from '../utils/constant';
 
 export const WEB_VITAL_EVENT_PLUGIN_ID = 'web-vitals';
@@ -25,20 +33,53 @@ export class WebVitalsPlugin extends InternalPlugin {
     // eslint-disable-next-line @typescript-eslint/no-empty-function
     configure(config: any): void {}
 
-    getWebVitalData(webVitalData: Metric, eventType: string): void {
-        const webVitalEvent:
-            | LargestContentfulPaintEvent
-            | FirstInputDelayEvent
-            | CumulativeLayoutShiftEvent = {
-            version: '1.0.0',
-            value: webVitalData.value
-        };
-        this.context?.record(eventType, webVitalEvent);
+    protected onload(): void {
+        onLCP((metric) => this.handleLCP(metric));
+        onFID((metric) => this.handleFID(metric));
+        onCLS((metric) => this.handleCLS(metric));
     }
 
-    protected onload(): void {
-        onLCP((data) => this.getWebVitalData(data, LCP_EVENT_TYPE));
-        onFID((data) => this.getWebVitalData(data, FID_EVENT_TYPE));
-        onCLS((data) => this.getWebVitalData(data, CLS_EVENT_TYPE));
+    handleLCP(metric: LCPMetricWithAttribution | Metric) {
+        const a = (metric as LCPMetricWithAttribution).attribution;
+        this.context?.record(LCP_EVENT_TYPE, {
+            version: '1.0.0',
+            value: metric.value,
+            attribution: {
+                element: a.element,
+                url: a.url,
+                timeToFirstByte: a.timeToFirstByte,
+                resourceLoadDelay: a.resourceLoadDelay,
+                resourceLoadTime: a.resourceLoadTime,
+                elementRenderDelay: a.elementRenderDelay
+            }
+        } as LargestContentfulPaintEvent);
+    }
+
+    handleCLS(metric: CLSMetricWithAttribution | Metric) {
+        const a = (metric as CLSMetricWithAttribution).attribution;
+        this.context?.record(CLS_EVENT_TYPE, {
+            version: '1.0.0',
+            value: metric.value,
+            attribution: {
+                largestShiftTarget: a.largestShiftTarget,
+                largestShiftValue: a.largestShiftValue,
+                largestShiftTime: a.largestShiftTime,
+                loadState: a.loadState
+            }
+        } as CumulativeLayoutShiftEvent);
+    }
+
+    handleFID(metric: FIDMetricWithAttribution | Metric) {
+        const a = (metric as FIDMetricWithAttribution).attribution;
+        this.context?.record(FID_EVENT_TYPE, {
+            version: '1.0.0',
+            value: metric.value,
+            attribution: {
+                eventTarget: a.eventTarget,
+                eventType: a.eventType,
+                eventTime: a.eventTime,
+                loadState: a.loadState
+            }
+        } as FirstInputDelayEvent);
     }
 }
