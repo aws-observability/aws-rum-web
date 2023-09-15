d7c90565ebaf7a52e765d7dafa6bf5a37388875f
Quinn Hanam <hanquinn@amazon.com>
2023-09-15 02:15:33 -0700

    fix: Reverse the order of ResourcePlugin integ tests

--- src/plugins/event-plugins/__integ__/ResourcePlugin.test.ts
+++ src/plugins/event-plugins/__integ__/ResourcePlugin.test.ts
@@ -8,6 +8,22 @@ fixture('ResourceEvent Plugin').page(
     'http://localhost:8080/delayed_image.html'
 );
 
+test('when resource loads after window.load then the resource is recorded', async (t: TestController) => {
+    await t
+        .wait(300)
+        .click(dispatch)
+        .expect(REQUEST_BODY.textContent)
+        .contains('BatchId');
+
+    const events = JSON.parse(await REQUEST_BODY.textContent).RumEvents.filter(
+        (e) =>
+            e.type === PERFORMANCE_RESOURCE_EVENT_TYPE &&
+            JSON.parse(e.details).targetUrl.includes('blank.png')
+    );
+
+    await t.expect(events.length).eql(1);
+});
+
 test('when resource loads before the plugin then the resource is recorded', async (t: TestController) => {
     await t
         .wait(300)
@@ -21,22 +37,6 @@ test('when resource loads before the plugin then the resource is recorded', asyn
             JSON.parse(e.details).targetUrl.includes(
                 'rum_javascript_telemetry.js'
             )
-    );
-
-    await t.expect(events.length).eql(1);
-});
-
-test('when resource loads after window.load then the resource is recorded', async (t: TestController) => {
-    await t
-        .wait(500)
-        .click(dispatch)
-        .expect(REQUEST_BODY.textContent)
-        .contains('BatchId');
-
-    const events = JSON.parse(await REQUEST_BODY.textContent).RumEvents.filter(
-        (e) =>
-            e.type === PERFORMANCE_RESOURCE_EVENT_TYPE &&
-            JSON.parse(e.details).targetUrl.includes('blank.png')
     );
 
     await t.expect(events.length).eql(1);
