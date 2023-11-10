166b8078123e7539855842362f7b1bc2d2907f2d
Quinn Hanam <hanquinn@amazon.com>
2023-11-10 12:55:14 -0800

    Refactor async functions and other fixes

--- src/plugins/event-plugins/TTIPlugin.ts
+++ src/plugins/event-plugins/TTIPlugin.ts
@@ -1,51 +1,42 @@
-import { TimeToInteractive } from '../../time-to-interactive/TimeToInteractive';
+import { TTIMetric, onTTI } from '../../time-to-interactive/TimeToInteractive';
 import { TimeToInteractiveEvent } from '../../events/time-to-interactive-event';
 import { TIME_TO_INTERACTIVE_EVENT_TYPE } from '../utils/constant';
 import { PluginContext } from 'plugins/types';
-import { isLongTaskSupported } from '../../utils/common-utils';
 import { InternalPlugin } from '../InternalPlugin';
 
 export const TTI_EVENT_PLUGIN_ID = 'time-to-interactive';
 
 export class TTIPlugin extends InternalPlugin {
-    // By default, FPS measurements are disabled to optimize performance
-    protected fpsEnabled = false;
+    protected fpsEnabled;
 
-    constructor(fpsMeasurementEnabled?: boolean) {
+    constructor(fpsMeasurementEnabled = false) {
         super(TTI_EVENT_PLUGIN_ID);
-        if (fpsMeasurementEnabled !== undefined) {
-            this.fpsEnabled = fpsMeasurementEnabled;
-        }
+        this.fpsEnabled = fpsMeasurementEnabled;
     }
 
     protected context!: PluginContext;
 
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    enable(): void {}
-
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    disable(): void {}
-
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
-    configure(config: any): void {}
-
-    load(context: PluginContext): void {
-        this.context = context;
-        const timeToInteractive: TimeToInteractive = new TimeToInteractive();
-
-        // If long task are not supported, TTI can't be computed for now
-        if (isLongTaskSupported()) {
-            timeToInteractive
-                .computeTimeToInteractive(this.fpsEnabled)
-                .then((ttiVal) => {
-                    this.context?.record(TIME_TO_INTERACTIVE_EVENT_TYPE, {
-                        version: '1.0.0',
-                        value: Math.round(ttiVal)
-                    } as TimeToInteractiveEvent);
-                })
-                .catch(() => {
-                    // If issue, don't record anything to handle gracefully
-                });
-        }
+    enable(): void {
+        /* Nothing to do. */
     }
+
+    disable(): void {
+        /* Nothing to do. */
+    }
+
+    configure(): void {
+        /* Nothing to do. */
+    }
+
+    onload(): void {
+        onTTI(this.handleTTI, { fpsEnabled: this.fpsEnabled });
+    }
+
+    private handleTTI = (metric: TTIMetric): void => {
+        const ttiEvent: TimeToInteractiveEvent = {
+            version: '1.0.0',
+            value: Math.round(metric.value)
+        };
+        this.context.record(TIME_TO_INTERACTIVE_EVENT_TYPE, ttiEvent);
+    };
 }
--- src/time-to-interactive/TimeToInteractive.ts
+++ src/time-to-interactive/TimeToInteractive.ts
@@ -1,4 +1,9 @@
-import { isFCPSupported, isLCPSupported } from '../utils/common-utils';
+/* eslint-disable jsdoc/check-indentation */
+import {
+    isFCPSupported,
+    isLCPSupported,
+    isLongTaskSupported
+} from '../utils/common-utils';
 import { onFCP, onLCP, Metric } from 'web-vitals';
 
 const LONG_TASK = 'longtask';
@@ -7,61 +12,69 @@ const NAVIGATION = 'navigation';
 const FCP = 'FCP';
 const LCP = 'LCP';
 
-/*
-What is Time to interactive (TTI)? 
+export interface TTIMetric {
+    name: 'TTI';
+    value: number;
+}
 
-Time to interactive is a a measurement to indicate how long it takes for a page to become interactive for the user. 
+export interface TTIReportOpts {
+    /**
+     * Enabling frames per second (FPS) tracking (@code{fpsEnabled: true}) may
+     * provide more accurate TTI, while incurring a small performance impact to
+     * the application.
+     *
+     * We expect that in most cases using FPS tracking is unnecessary. A period
+     * with no long tasks (which ARE tracked) will typically have a high FPS.
+     */
+    fpsEnabled: boolean;
+}
 
+export type TTIReportCallback = (
+    metric: TTIMetric,
+    opts?: TTIReportOpts
+) => void;
 
-How is TTI computed? 
+export function onTTI(onReport: TTIReportCallback, opts: TTIReportOpts) {
+    if (isLongTaskSupported()) {
+        new TimeToInteractive(onReport, opts);
+    }
+}
 
-This implements the TTI  algorithm with some modifications. 
-(Ref: https://akamai.github.io/boomerang/oss/BOOMR.plugins.Continuity.html)
+/**
+ * This class measures the "time to interactive" web vital. Time to interactive
+ * is a measure of how long it takes for a page to become interactive for the
+ * user.
+ * <p>
+ * This class implements the TTI algorithm used by Boomerang, with modifications.
+ * (Ref: https://akamai.github.io/boomerang/oss/BOOMR.plugins.Continuity.html)
+ * <p>
+ * Steps to TTI calculation:
+ * 1) Find visually ready timestamp (highest of domcontentLoadedEnd, FCP or LCP).
+ * 2) Starting from the visually ready timestamp, find a 500ms quiet window. A
+ *    quiet window has the following characteristics:
+ *      a) No Long Tasks
+ *      b) FPS is above 20 (if enabled)
+ * 3) TTI is recorded as visually ready timestamp + time from visually ready to the start of the quiet window.
+ * <p>
+ * This class measures TTI only when running in a browser that supports Long Tasks.
+ */
+class TimeToInteractive {
+    private onReport: TTIReportCallback;
 
-Steps to TTI calculation: 
-1) Find visually ready timestamp (highest of domcontentLoadedEnd(if available), FCP(if available), LCP(if available))
-2) Starting from the Visually Ready timestamp, find a 500ms quiet window. A quiet window has the following characteristics: 
-a) No Long Tasks 
-b) FPS is above 20 (if opted into)
-
-TTI's measurement interval is every 100ms. Each check is performed and bucketed into a 100ms interval. 
-
-3) TTI is recorded as Visually ready timestamp + time from visually ready to the start of the quiet window
-
-
-How is Frames per second (FPS) measured? 
-
-The frames per second listener measures frames per second. It does so using requestAnimationFrame. 
-This works by increasing the frame for each call in a given time bucket. For most devices and application this 
-will be 60. 
-
-
-Are there any performance implications of listening for frames per second? 
-
-Yes, there can be minimal performance implications if it disrupts the main thread of application. 
-This should not happen in most cases, but can be performance impacting when it does happen. There is some research 
-ongoing to more efficiently capture this via PerformanceObserver but there is no timeline on when this will be widely 
-available. 
-
-In order to lessen any potential performance impacts on consumer web applications, we decided to have FPS as an optional
-opt in measurement. If consumers see performance impacts on their application, they can disable FPS for their TTI measurements. 
-In most cases, long tasks should be suffice to capture the spirit of FPS since a quiet period with no long tasks will in most cases 
-have a FPS above the threshold.
-
-How is TTI support?
-
-TTI can only be computed for now for browsers that support Long Tasks. 
-
-Refer to this thread for more context on decision making: https://github.com/aws-observability/aws-rum-web/pull/465#discussion_r1382470863
-*/
-
-export class TimeToInteractive {
     private fcpTime?: number;
     private lcpTime?: number;
     private domContentLoadedEventEnd?: number;
-    private visuallyReadyTimestamp: number | null = null;
+    private visuallyReadyTimestamp = 0;
+    private timeIntervals = 0;
+    private visualReadyInterval: NodeJS.Timeout;
     private ttiTracker: any = {};
     private ttiResolved = false;
+
+    private ttiInterval!: NodeJS.Timeout;
+    private startBucket = 0;
+    private currBucket = 0;
+    private acceptedIntervals = 0;
+    private totalIntervals = 0;
 
     private fcpSupported = false;
     private lcpSupported = false;
@@ -76,132 +89,105 @@ export class TimeToInteractive {
     private FPS_THRESHOLD = 20 / (1000 / this.COLLECTION_PERIOD);
     private LONG_TASK_THRESHOLD = 0;
 
-    public async computeTimeToInteractive(
-        fpsEnabled: boolean
-    ): Promise<number> {
-        this.fpsEnabled = fpsEnabled;
+    constructor(onReport: TTIReportCallback, opts: TTIReportOpts) {
+        this.onReport = onReport;
+        this.fpsEnabled = opts.fpsEnabled;
         this.initListeners();
-        return new Promise<number>((resolve, reject) => {
-            this.checkForVisualReady().then((visuallyReadyTimestamp) => {
-                this.computeTTI(visuallyReadyTimestamp).then((ttiVal) => {
-                    resolve(ttiVal);
-                });
-            });
-        });
+        this.visualReadyInterval = this.startVisualReadyInterval();
     }
 
-    async checkForVisualReady(): Promise<number> {
-        // Check if visually ready
-        return new Promise<number>((resolve, reject) => {
-            let timeIntervals = 0;
-            const visuallyReadyInterval: any = setInterval(() => {
-                const isCheckTimedOut: boolean =
-                    timeIntervals * this.CHECK_PERIOD >
-                    this.VISUALLY_READY_RESOLVE_TIMEOUT;
+    private visualReadyIntervalHandler = () => {
+        const isCheckTimedOut: boolean =
+            this.timeIntervals * this.CHECK_PERIOD >
+            this.VISUALLY_READY_RESOLVE_TIMEOUT;
 
-                if (this.isOkToResolveVisuallyReady(isCheckTimedOut)) {
-                    clearInterval(visuallyReadyInterval);
+        if (this.isOkToResolveVisuallyReady(isCheckTimedOut)) {
+            clearInterval(this.visualReadyInterval);
 
-                    this.visuallyReadyTimestamp = Math.max(
-                        this.fcpTime ? this.fcpTime : 0,
-                        this.lcpTime ? this.lcpTime : 0,
-                        this.domContentLoadedEventEnd
-                            ? this.domContentLoadedEventEnd
-                            : 0
-                    );
-                    resolve(this.visuallyReadyTimestamp);
-                } else {
-                    // No VR timestamps and check timed out, therefore stop searching
-                    if (isCheckTimedOut) {
-                        clearInterval(visuallyReadyInterval);
-                        this.ttiResolved = true;
-                        this.ttiTracker = {};
-                        reject(
-                            'Insufficient visually ready timestamps to compute TTI'
-                        );
-                    }
-                }
-                timeIntervals += 1;
-            }, this.CHECK_PERIOD);
-        });
-    }
-
-    async computeTTI(visuallyReadyTimestamp: number): Promise<number> {
-        return new Promise<number>((resolve, reject) => {
-            const startBucket = Math.max(
-                this.computeTimeWindow(visuallyReadyTimestamp),
-                0
+            this.visuallyReadyTimestamp = Math.max(
+                this.fcpTime ? this.fcpTime : 0,
+                this.lcpTime ? this.lcpTime : 0,
+                this.domContentLoadedEventEnd
+                    ? this.domContentLoadedEventEnd
+                    : 0
             );
+            this.ttiInterval = this.startTtiInterval();
+        } else {
+            // No VR timestamps and check timed out, therefore stop searching
+            if (isCheckTimedOut) {
+                clearInterval(this.visualReadyInterval);
+                this.ttiResolved = true;
+                this.ttiTracker = {};
+            }
+        }
+        this.timeIntervals += 1;
+    };
 
-            let currBucket = startBucket;
-            let acceptedIntervals = 0;
-            let totalIntervals = 0;
+    private ttiIntervalHandler = () => {
+        const isTTITimedOut =
+            this.totalIntervals * this.CHECK_PERIOD > this.TTI_RESOLVE_TIMEOUT;
 
-            const tti: any = setInterval(() => {
-                const isTTITimedOut =
-                    totalIntervals * this.CHECK_PERIOD >
-                    this.TTI_RESOLVE_TIMEOUT;
+        if (isTTITimedOut) {
+            // TTI computation timed out. Don't attempt any more calculations
+            this.ttiResolved = true;
+            this.ttiTracker = {};
+            clearInterval(this.ttiInterval);
+        }
+        const endBucket = this.computeTimeWindow();
 
-                if (isTTITimedOut) {
-                    // TTI computation timed out. Don't attempt any more calculations
-                    this.ttiResolved = true;
-                    this.ttiTracker = {};
-                    reject('TTI computation timed out');
-                }
-                const endBucket = this.computeTimeWindow();
+        for (let bucket = this.currBucket; bucket <= endBucket; bucket++) {
+            this.currBucket = bucket;
+            let allTTIConditionsFulfiled = true;
 
-                for (let bucket = currBucket; bucket <= endBucket; bucket++) {
-                    currBucket = bucket;
-                    let allTTIConditionsFulfiled = true;
+            // Check long task fulfils criteria
+            if (this.isTTIConditionNotFulfilied(LONG_TASK, bucket)) {
+                allTTIConditionsFulfiled = false;
+            }
 
-                    // Check long task fulfils criteria
-                    if (this.isTTIConditionNotFulfilied(LONG_TASK, bucket)) {
-                        allTTIConditionsFulfiled = false;
-                    }
+            // Check FPS fulfils criteria
+            if (
+                this.fpsEnabled &&
+                this.isTTIConditionNotFulfilied(FPS, bucket)
+            ) {
+                allTTIConditionsFulfiled = false;
+            }
 
-                    // Check FPS fulfils criteria
-                    if (
-                        this.fpsEnabled &&
-                        this.isTTIConditionNotFulfilied(FPS, bucket)
-                    ) {
-                        allTTIConditionsFulfiled = false;
-                    }
+            // If all conditions for a time window are not fulfiled, reset and move to next time window
+            if (!allTTIConditionsFulfiled) {
+                this.acceptedIntervals = 0;
+                continue;
+            }
 
-                    // If all conditions for a time window are not fulfiled, reset and move to next time window
-                    if (!allTTIConditionsFulfiled) {
-                        acceptedIntervals = 0;
-                        continue;
-                    }
+            // All conditions were met so its an accepted interval towards quiet window quota
+            this.acceptedIntervals += 1;
 
-                    // All conditions were met so its an accepted interval towards quiet window quota
-                    acceptedIntervals += 1;
+            if (this.isTTIResolved(this.acceptedIntervals)) {
+                this.ttiResolved = true;
+                clearInterval(this.ttiInterval);
+                this.onReport({
+                    name: 'TTI',
+                    value: this.computeTTIValue()
+                });
+                break;
+            } else {
+                // TTI not yet resolvable, so continue trying
+                continue;
+            }
+        }
 
-                    if (this.isTTIResolved(acceptedIntervals)) {
-                        this.ttiResolved = true;
-                        clearInterval(tti);
-                        resolve(
-                            this.computeTTIValue(
-                                currBucket,
-                                startBucket,
-                                visuallyReadyTimestamp
-                            )
-                        );
-                        break;
-                    } else {
-                        // TTI not yet resolvable, so continue trying
-                        continue;
-                    }
-                }
+        this.totalIntervals += 1;
+    };
 
-                totalIntervals += 1;
-            }, this.CHECK_PERIOD);
-        });
+    private startTtiInterval(): NodeJS.Timeout {
+        this.startBucket = Math.max(
+            this.computeTimeWindow(this.visuallyReadyTimestamp),
+            0
+        );
+        this.currBucket = this.startBucket;
+        this.acceptedIntervals = 0;
+        this.totalIntervals = 0;
+        return setInterval(() => this.ttiIntervalHandler, this.CHECK_PERIOD);
     }
-
-    /*
-    Helper functions 
-
-    */
 
     private isOkToResolveVisuallyReady(isCheckTimedOut: boolean): boolean {
         /*
@@ -251,22 +237,21 @@ export class TimeToInteractive {
             return false;
         }
     }
-    private computeTTIValue(
-        currBucket: number,
-        startBucket: number,
-        visuallyReadyTimestamp: number
-    ): number {
+    private computeTTIValue(): number {
         /*
         TTI is the time to the start of the TTI quiet window. 
         Therefore, it is the sum of the time to visually ready + time from VR to the start of the quiet window
         */
         const timeToQuietPeriodFromVisuallyReady: number =
-            (currBucket - this.REQUIRED_ACCEPTED_INTERVALS - startBucket + 1) *
+            (this.currBucket -
+                this.REQUIRED_ACCEPTED_INTERVALS -
+                this.startBucket +
+                1) *
             this.COLLECTION_PERIOD;
         // Cleanup
         this.ttiTracker = {};
 
-        return visuallyReadyTimestamp + timeToQuietPeriodFromVisuallyReady;
+        return this.visuallyReadyTimestamp + timeToQuietPeriodFromVisuallyReady;
     }
 
     private isTTIConditionNotFulfilied(
@@ -320,6 +305,10 @@ export class TimeToInteractive {
         if (this.fpsEnabled && window.requestAnimationFrame !== undefined) {
             this.framesPerSecondListener();
         }
+    }
+
+    private startVisualReadyInterval(): NodeJS.Timeout {
+        return setInterval(this.visualReadyIntervalHandler, this.CHECK_PERIOD);
     }
 
     private computeTimeWindow(currTime?: number): number {
@@ -383,10 +372,11 @@ export class TimeToInteractive {
                 }
                 list.getEntries()
                     .filter((e) => e.entryType === NAVIGATION)
+                    .map((e) => e as PerformanceNavigationTiming)
                     .forEach((event) => {
-                        if (event.toJSON().domContentLoadedEventEnd) {
+                        if (event.domContentLoadedEventEnd) {
                             this.domContentLoadedEventEnd =
-                                event.toJSON().domContentLoadedEventEnd;
+                                event.domContentLoadedEventEnd;
                         }
                     });
             }
@@ -397,10 +387,11 @@ export class TimeToInteractive {
         });
     };
 
-    /*
-    Track frames per second 
-    */
     framesPerSecondListener = () => {
+        // The frames per second listener measures frames per second. It does so
+        // using requestAnimationFrame.  This works by increasing the frame for
+        // each call in a given time bucket. For most devices and application
+        // this will be 60.
         const trackFrames = () => {
             // increment FPS by 1 in tracker for time window
             this.addToTracker(FPS, this.computeTimeWindow(), 1);
