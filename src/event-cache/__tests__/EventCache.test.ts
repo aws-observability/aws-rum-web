09673278631979c14c4327a5eb17cb00c3393fe3
Paras Sakharkar <sakparas@amazon.com>
2024-01-31 17:06:26 -0800

    chore(release): 1.17.0

--- src/event-cache/__tests__/EventCache.test.ts
+++ src/event-cache/__tests__/EventCache.test.ts
@@ -31,7 +31,7 @@ jest.mock('../../sessions/SessionManager', () => ({
     }))
 }));
 
-const WEB_CLIENT_VERSION = '1.16.0';
+const WEB_CLIENT_VERSION = '1.17.0';
 
 describe('EventCache tests', () => {
     beforeAll(() => {
