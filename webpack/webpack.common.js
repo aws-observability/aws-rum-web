2e3eacc945ed5a2f2dc9ff692e79964bf0938b00
Billy Zhou <billyzh@amazon.com>
2024-06-21 11:36:44 -0700

    feat: reduce package size to 148kb by fixing tree shaking

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,16 +1,18 @@
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 const babelLoaderOptions = {
-    presets: [['@babel/preset-env', {}]],
-    plugins: [
-        '@babel/plugin-transform-modules-commonjs',
+    presets: [
         [
-            '@babel/plugin-transform-runtime',
+            '@babel/preset-env',
             {
-                absoluteRuntime: false,
-                corejs: false,
-                helpers: true,
-                regenerator: true
+                targets: {
+                    edge: '17',
+                    firefox: '60',
+                    chrome: '67',
+                    safari: '11.1'
+                }
+                // useBuiltIns: 'usage',
+                // corejs: '3.6.5'
             }
         ]
     ]
