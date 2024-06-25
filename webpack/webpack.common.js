06f81c5525e1692698279f6dc084b3b3df60baa1
Billy Zhou <billyzh@amazon.com>
2024-06-25 11:06:06 -0700

    feat: default build targets

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,19 +1,7 @@
 const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 const babelLoaderOptions = {
-    presets: [
-        [
-            '@babel/preset-env',
-            {
-                targets: {
-                    edge: '17',
-                    firefox: '60',
-                    chrome: '67',
-                    safari: '11.1'
-                }
-            }
-        ]
-    ]
+    presets: [['@babel/preset-env']]
 };
 
 module.exports = {
